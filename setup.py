8592a9469e703e07d85a74b88a437783cef65e12
ivm <you@example.com>
2024-09-13 15:27:14 +0200

    update

--- setup.py
+++ setup.py
@@ -14,13 +14,8 @@ setup(
                 'src/droid_kernels.cu'
             ],
             extra_compile_args={
-                'cxx': ['-O3'],
-                'nvcc': ['-O3',
-                    '-gencode=arch=compute_60,code=sm_60',
-                    '-gencode=arch=compute_61,code=sm_61',
-                    '-gencode=arch=compute_70,code=sm_70',
-                    '-gencode=arch=compute_75,code=sm_75',
-                    '-gencode=arch=compute_80,code=sm_80',
+                'cxx': ['-O1', '-g'],
+                'nvcc': ['-O1', '-g', '-G', '-Xcompiler=-fopenmp',
                     '-gencode=arch=compute_86,code=sm_86',
                 ]
             }),
---
+++ test.py
@@ -0,0 +1,9 @@
+
+import torch
+import droid_backends  # Le nom de l'extension compilée
+
+a = torch.ones((32, 64), device='cuda')  # Créer des tenseurs sur le GPU
+b = torch.ones((32, 64), device='cuda')
+
+c = droid_backends.depth_filter(a, b)  # Appeler la fonction CUDA
+print(c)  # Résultat de l'addition
